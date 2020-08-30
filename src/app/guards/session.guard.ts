b56872fd56e4ca7dd3d074dd0cc954fdff75d7dd
Diegoher <diegoherr92@gmail.com>
2020-08-29 19:24:52 -0500

    adding session guard

--- src/app/guards/session.guard.ts
+++ src/app/guards/session.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../core/services/auth/auth.service';;
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.hasSession().pipe(
+      map(user => user === null ? false : true)
+    );
   }
-  
 }
