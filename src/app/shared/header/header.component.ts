0fef1238bb3e2f2cd5089f0c497e61c7777193a8
Diegoher <diegoherr92@gmail.com>
2020-08-29 19:13:52 -0500

    creating layout page structure

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  sessionButton: boolean;
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.auth.hasSession().subscribe((session) => {
+      if (session) {
+        this.sessionButton = true;
+      } else {
+        this.sessionButton = false;
+      }
+    });
   }
 
+  logOut() {
+    this.auth.logOut().then(() => {
+      this.router.navigate(['/home']);
+    });
+  }
+
+  logIn() {
+    this.auth.logOut().then(() => {
+      this.router.navigate(['/auth/login']);
+    });
+  }
 }
