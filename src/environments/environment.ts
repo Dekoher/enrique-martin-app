bbc812b482018be33f25f4561e7b8a29001f8e57
Diegoher <diegoherr92@gmail.com>
2020-09-01 15:54:56 -0500

    adding firebase environment variables object

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,15 @@
 export const environment = {
   production: false,
   urlBase: process.env.URL_BASE,
+  firebase: {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATA_BASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+  },
 };
 
 /*
